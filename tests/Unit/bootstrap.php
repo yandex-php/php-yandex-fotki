42f61115df14fa3cf3f1ec249773cf36d4e0794f
Kulikov Roman <flinnraider@yandex.ru>
2015-10-31 23:20:42 +0900

    see #6 - поправил настройку констант

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -10,6 +10,6 @@ $config = require __DIR__ . '/config.php';
 //@formatter:off
 define( 'FOTKI_API_LOGIN',                    (string)$config['login'],                 true );
 define( 'FOTKI_API_OAUTH_TOKEN',              (string)$config['oauthToken'],            true );
-define( 'FOTKI_API_DELETE_STUFF_AFTER_TESTS', (string)$config['deleteStuffAfterTests'], true );
+define( 'FOTKI_API_DELETE_STUFF_AFTER_TESTS',   (bool)$config['deleteStuffAfterTests'], true );
 define( 'FOTKI_API_ASSETS',                           __DIR__ . '/../assets',           true );
 //@formatter:on
