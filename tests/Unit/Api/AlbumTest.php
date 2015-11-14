c3af74c538a96a926dc31829dee99b763c6231af
Kulikov Roman <flinnraider@yandex.ru>
2015-11-14 21:06:15 +0300

    see #11 - Добавил тесты для новых методов альбома, создал BaseTestCase

---
+++ tests/Unit/Api/AlbumTest.php
@@ -0,0 +1,190 @@
+<?php
+/**
+ * Author: Kulikov Roman
+ * Email: flinnraider@yandex.ru
+ */
+
+namespace Yandex\Fotki\Tests\Unit\Api;
+
+
+use Yandex\Fotki\Tests\Unit\BaseTestCase;
+
+class AlbumTest extends BaseTestCase {
+
+	public function testAlbumGetParent() {
+		$parent = $this->api->createAlbum( array( 'title' => 'testAlbumGetParent Parent' ) )->load();
+		$child  = $this->api->createAlbum( array( 'title' => 'testAlbumGetParent Child' ), $parent->getId() )->load();
+
+		$this->assertEquals( null, $parent->getParent() );
+		$this->assertEquals( null, $parent->getParentId() );
+		$this->assertEquals( null, $parent->getApiUrlParent() );
+		$this->assertEquals( $parent->getId(), $child->getParent()->getId() );
+		$this->assertEquals( $parent->getId(), $child->getParentId() );
+		$this->assertEquals( parse_url( $parent->getApiUrl(), PHP_URL_PATH ), parse_url( $child->getApiUrlParent(), PHP_URL_PATH ) );
+	}
+
+	public function testAlbumSetParent() {
+		$parent = $this->api->createAlbum( array( 'title' => 'testAlbumSetParent Parent' ) )->load();
+		$child  = $this->api->createAlbum( array( 'title' => 'testAlbumSetParent Child' ) )->load();
+
+		$child->setParent( $parent );
+		$this->assertEquals( 'testAlbumSetParent Parent', $child->getParent()->getTitle() );
+		$this->assertEquals( $parent->getId(), $child->getParentId() );
+		$this->assertEquals( parse_url( $parent->getApiUrl(), PHP_URL_PATH ), parse_url( $child->getParent()->getApiUrl(), PHP_URL_PATH ) );
+		$this->assertEquals( parse_url( $parent->getApiUrl(), PHP_URL_PATH ), parse_url( $child->getApiUrlParent(), PHP_URL_PATH ) );
+
+		$child->setParent( null );
+		$this->assertEquals( null, $child->getParent() );
+		$this->assertEquals( null, $child->getParentId() );
+		$this->assertEquals( null, $child->getApiUrlParent() );
+
+		$child->setParent( $parent->getId() );
+		$this->assertEquals( 'testAlbumSetParent Parent', $child->getParent()->getTitle() );
+		$this->assertEquals( $parent->getId(), $child->getParentId() );
+		$this->assertEquals( parse_url( $parent->getApiUrl(), PHP_URL_PATH ), parse_url( $child->getParent()->getApiUrl(), PHP_URL_PATH ) );
+		$this->assertEquals( parse_url( $parent->getApiUrl(), PHP_URL_PATH ), parse_url( $child->getApiUrlParent(), PHP_URL_PATH ) );
+	}
+
+	/**
+	 * Так как возможность получить дочерние альбомы есть только на
+	 * верхнем уровне Api, то метод \Yandex\Fotki\Api\Album::getChildren
+	 * возвращает текущее состояние объекта, а не загружает его
+	 * детей с сервера Api.
+	 *
+	 * Может в будущем это будет исправлено. Но пока тестировать тут нечего
+	 *
+	 * @throws \Yandex\Fotki\Exception\Api\Album
+	 */
+	public function testAlbumGetChildren() {
+//		$parent = $this->api->createAlbum( array( 'title' => 'testAlbumGetChildren Parent' ) )->load();
+//		$child1 = $this->api->createAlbum( array( 'title' => 'testAlbumGetChildren Child1' ), $parent->getId() )->load();
+//		$child2 = $this->api->createAlbum( array( 'title' => 'testAlbumGetChildren Child2' ), $parent->getId() )->load();
+//
+//		$children = $parent->getChildren();
+//		$this->assertEquals('testAlbumGetChildren Child1', $children[$child1->getId()]->getTitle());
+//		$this->assertEquals('testAlbumGetChildren Child2', $children[$child2->getId()]->getTitle());
+	}
+
+	public function testAlbumSetChildren() {
+		$parent = $this->api->createAlbum( array( 'title' => 'testAlbumSetChildren Parent' ) )->load();
+		$child1 = $this->api->createAlbum( array( 'title' => 'testAlbumSetChildren Child1' ), $parent->getId() )->load();
+		$child2 = $this->api->createAlbum( array( 'title' => 'testAlbumSetChildren Child2' ), $parent->getId() )->load();
+
+		$parent->setChildren( array( $child1, $child2 ) );
+		$children = $parent->getChildren();
+		$this->assertEquals( 2, count( $children ) );
+		$this->assertEquals( 'testAlbumSetChildren Child1', $children[ $child1->getId() ]->getTitle() );
+		$this->assertEquals( 'testAlbumSetChildren Child2', $children[ $child2->getId() ]->getTitle() );
+
+		$parent->setChildren( array( intval( $child1->getId() ), intval( $child2->getId() ) ) );
+		$children = $parent->getChildren();
+		$this->assertEquals( 2, count( $children ) );
+		$this->assertEquals( 'testAlbumSetChildren Child1', $children[ $child1->getId() ]->getTitle() );
+		$this->assertEquals( 'testAlbumSetChildren Child2', $children[ $child2->getId() ]->getTitle() );
+
+		$parent->setChildren( array( strval( $child1->getId() ), strval( $child2->getId() ) ) );
+		$children = $parent->getChildren();
+		$this->assertEquals( 2, count( $children ) );
+		$this->assertEquals( 'testAlbumSetChildren Child1', $children[ $child1->getId() ]->getTitle() );
+		$this->assertEquals( 'testAlbumSetChildren Child2', $children[ $child2->getId() ]->getTitle() );
+	}
+
+	public function testAlbumAddChild() {
+		$parent = $this->api->createAlbum( array( 'title' => 'testAlbumAddChild Parent' ) )->load();
+		$child1 = $this->api->createAlbum( array( 'title' => 'testAlbumAddChild Child1' ), $parent->getId() )->load();
+		$child2 = $this->api->createAlbum( array( 'title' => 'testAlbumAddChild Child2' ), $parent->getId() )->load();
+		$child3 = $this->api->createAlbum( array( 'title' => 'testAlbumAddChild Child3' ), $parent->getId() )->load();
+
+		$this->assertEquals( 0, count( $parent->getChildren() ) );
+
+		$parent->addChild( $child1 );
+		$children = $parent->getChildren();
+		$this->assertEquals( 1, count( $children ) );
+		$this->assertEquals( 'testAlbumAddChild Child1', $children[ $child1->getId() ]->getTitle() );
+
+		$parent->addChild( intval( $child2->getId() ) );
+		$children = $parent->getChildren();
+		$this->assertEquals( 2, count( $children ) );
+		$this->assertEquals( 'testAlbumAddChild Child1', $children[ $child1->getId() ]->getTitle() );
+		$this->assertEquals( 'testAlbumAddChild Child2', $children[ $child2->getId() ]->getTitle() );
+
+		$parent->addChild( strval( $child3->getId() ) );
+		$children = $parent->getChildren();
+		$this->assertEquals( 3, count( $children ) );
+		$this->assertEquals( 'testAlbumAddChild Child1', $children[ $child1->getId() ]->getTitle() );
+		$this->assertEquals( 'testAlbumAddChild Child2', $children[ $child2->getId() ]->getTitle() );
+		$this->assertEquals( 'testAlbumAddChild Child3', $children[ $child3->getId() ]->getTitle() );
+
+		$parent->addChild( null );
+		$children = $parent->getChildren();
+		$this->assertEquals( 3, count( $children ) );
+		$this->assertEquals( 'testAlbumAddChild Child1', $children[ $child1->getId() ]->getTitle() );
+		$this->assertEquals( 'testAlbumAddChild Child2', $children[ $child2->getId() ]->getTitle() );
+		$this->assertEquals( 'testAlbumAddChild Child3', $children[ $child3->getId() ]->getTitle() );
+	}
+
+	public function testAlbumRemoveChild() {
+		$parent = $this->api->createAlbum( array( 'title' => 'testAlbumRemoveChild Parent' ) )->load();
+		$child1 = $this->api->createAlbum( array( 'title' => 'testAlbumRemoveChild Child1' ), $parent->getId() )->load();
+		$child2 = $this->api->createAlbum( array( 'title' => 'testAlbumRemoveChild Child2' ), $parent->getId() )->load();
+		$child3 = $this->api->createAlbum( array( 'title' => 'testAlbumRemoveChild Child3' ), $parent->getId() )->load();
+
+		$parent->setChildren( array(
+			$child1,
+			$child2,
+			$child3,
+		) );
+		$children = $parent->getChildren();
+		$this->assertEquals( 3, count( $parent->getChildren() ) );
+		$this->assertEquals( true, isset( $children[ $child1->getId() ] ) );
+		$this->assertEquals( true, isset( $children[ $child2->getId() ] ) );
+		$this->assertEquals( true, isset( $children[ $child3->getId() ] ) );
+
+		$parent->removeChild( null );
+		$children = $parent->getChildren();
+		$this->assertEquals( 3, count( $parent->getChildren() ) );
+		$this->assertEquals( true, isset( $children[ $child1->getId() ] ) );
+		$this->assertEquals( true, isset( $children[ $child2->getId() ] ) );
+		$this->assertEquals( true, isset( $children[ $child3->getId() ] ) );
+
+		$parent->removeChild( $child1 );
+		$children = $parent->getChildren();
+		$this->assertEquals( 2, count( $children ) );
+		$this->assertEquals( false, isset( $children[ $child1->getId() ] ) );
+		$this->assertEquals( true, isset( $children[ $child2->getId() ] ) );
+		$this->assertEquals( true, isset( $children[ $child3->getId() ] ) );
+
+		$parent->removeChild( intval( $child2->getId() ) );
+		$children = $parent->getChildren();
+		$this->assertEquals( 1, count( $children ) );
+		$this->assertEquals( false, isset( $children[ $child1->getId() ] ) );
+		$this->assertEquals( false, isset( $children[ $child2->getId() ] ) );
+		$this->assertEquals( true, isset( $children[ $child3->getId() ] ) );
+
+		$parent->removeChild( strval( $child3->getId() ) );
+		$children = $parent->getChildren();
+		$this->assertEquals( 0, count( $children ) );
+		$this->assertEquals( false, isset( $children[ $child1->getId() ] ) );
+		$this->assertEquals( false, isset( $children[ $child2->getId() ] ) );
+		$this->assertEquals( false, isset( $children[ $child3->getId() ] ) );
+	}
+
+	public function testAlbumRemoveAllChildren() {
+		$parent = $this->api->createAlbum( array( 'title' => 'testAlbumRemoveAllChildren Parent' ) )->load();
+		$child1 = $this->api->createAlbum( array( 'title' => 'testAlbumRemoveAllChildren Child1' ), $parent->getId() )->load();
+		$child2 = $this->api->createAlbum( array( 'title' => 'testAlbumRemoveAllChildren Child2' ), $parent->getId() )->load();
+		$child3 = $this->api->createAlbum( array( 'title' => 'testAlbumRemoveAllChildren Child3' ), $parent->getId() )->load();
+
+		$parent->setChildren( array(
+			$child1,
+			$child2,
+			$child3,
+		) );
+
+		$this->assertEquals( 3, count( $parent->getChildren() ) );
+
+		$parent->removeAllChildren();
+
+		$this->assertEquals( 0, count( $parent->getChildren() ) );
+	}
+}
--- tests/Unit/Api/ApiTest.php
+++ tests/Unit/Api/ApiTest.php
@@ -10,11 +10,9 @@ namespace Yandex\Fotki\Tests\Unit\Api;
 use Yandex\Fotki\Api;
 use Yandex\Fotki\Exception\Api\Album;
 use Yandex\Fotki\Exception\Api\Photo;
+use Yandex\Fotki\Tests\Unit\BaseTestCase;
 
-class ApiTest extends \PHPUnit_Framework_TestCase {
-
-	/** @var \Yandex\Fotki\Api */
-	public $api;
+class ApiTest extends BaseTestCase {
 
 	public function testAlbumCreate() {
 		$album = $this->api->createAlbum( array(
@@ -344,66 +342,5 @@ class ApiTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals( 'testAlbumsDirectTree root.category1.product1', $tree[ $product1->getId() ]->getTitle() );
 		$this->assertEquals( 'testAlbumsDirectTree root.category1.product2', $tree[ $product2->getId() ]->getTitle() );
 		$this->assertEquals( 'testAlbumsDirectTree root.category1.product3', $tree[ $product3->getId() ]->getTitle() );
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	protected function setUp() {
-		parent::setUp();
-
-		$this->api = new Api( FOTKI_API_LOGIN );
-		$this->api->oauth( FOTKI_API_OAUTH_TOKEN );
-	}
-
-	/**
-	 * @inheritdoc
-	 * @throws \Yandex\Fotki\Exception\Api\DangerousAlbumDeleting
-	 */
-	protected function tearDown() {
-		if ( FOTKI_API_DELETE_STUFF_AFTER_TESTS === false ) {
-			parent::tearDown();
-
-			return;
-		}
-
-		$api = $this->api;
-		/** @var \Yandex\Fotki\Api\Album[] $albums */
-		$albums = $api->getAlbumsCollection()->loadAll()->getList();
-		foreach ( $albums as $album ) {
-			if ( stripos( $album->getTitle(), 'test' ) === false ) {
-				continue;
-			}
-
-			try {
-				$api->deleteAlbum( $album->getId(), $api::DELETE_ALBUM_WITH_PHOTOS_YES, $api::DELETE_ALBUM_WITH_CHILDREN_ALBUMS_YES );
-			} catch ( Album $e ) {
-				if ( $e->getCode() == 404 ) {
-					continue;
-				} else {
-					throw new \RuntimeException( "", 0, $e );
-				}
-			}
-		}
-
-		/** @var \Yandex\Fotki\Api\Photo[] $photos */
-		$photos = $api->getPhotosCollection()->loadAll()->getList();
-		foreach ( $photos as $photo ) {
-			if ( stripos( $photo->getTitle(), 'test' ) === false ) {
-				continue;
-			}
-
-			try {
-				$api->deletePhoto( $photo->getId() );
-			} catch ( Photo $e ) {
-				if ( $e->getCode() == 404 ) {
-					continue;
-				} else {
-					throw new \RuntimeException( "", 0, $e );
-				}
-			}
-		}
-
-		parent::tearDown();
 	}
 }
